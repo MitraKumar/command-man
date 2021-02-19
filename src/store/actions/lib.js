28dab0ac1a13a8bf795406abcccc285c879ffd33
kkmitra <kaushik.mitra@innoraft.com>
2021-02-19 14:45:49 +0530

    Updated the commands to easier movement.
    - Added edge case to check that the player doesn't move across the wall.

--- src/store/actions/lib.js
+++ src/store/actions/lib.js
@@ -10,8 +10,17 @@ export class CommandHandler {
 
   handle(command, args) {
     switch(command) {
-      case "move":
-        return this.handleMove(...args);
+      case "left":
+        return this.handleXMove(-1, ...args);
+        break;
+      case "right":
+        return this.handleXMove(1, ...args);
+        break;
+      case "up":
+        return this.handleYMove(-1, ...args);
+        break;
+      case "down":
+        return this.handleYMove(1, ...args);
         break;
       default:
         this.dispatch({
@@ -28,17 +37,52 @@ export class CommandHandler {
     }
   }
 
+  handleXMove(direction, command_name, xdiff) {
+    this.handleMove(command_name, Number(xdiff) * direction, 0)
+  }
+
+  handleYMove(direction, command_name, ydiff) {
+    this.handleMove(command_name, 0, Number(ydiff) * direction)
+  }
+
+  checkForWallInXdir(lower_bound, upper_bound, y_position) {
+    for (let i = lower_bound; i <= upper_bound; i++) {
+      if (this.maze[y_position][i] == 1) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkForWallInYdir(lower_bound, upper_bound, x_position) {
+    for (let i = lower_bound; i <= upper_bound; i++) {
+      if (this.maze[i][x_position] == 1) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   handleMove(command_name, xdiff = 0, ydiff = 0) {
 
     const x = this.x + Number(xdiff);
     const y = this.y + Number(ydiff);
+
+    let command_string = "";
+    if (ydiff === 0) {
+      command_string = `${command_name} ${Math.abs(xdiff)}`
+    } else if (xdiff === 0) {
+      command_string = `${command_name} ${Math.abs(ydiff)}`
+    } else {
+      command_string = `${command_name} ${xdiff} ${ydiff}`
+    }
 
 
     if (x < 0 || y < 0 || x >= this.maze[0].length || y >= this.maze.length) {
       this.dispatch({
         type: "UPDATE_HISTORY",
         payload: {
-          command: `${command_name} ${xdiff} ${ydiff}`,
+          command: command_string,
           value: {
             type: 'error',
             value: `Moving outside the world.`
@@ -48,22 +92,70 @@ export class CommandHandler {
       return;
     }
 
-    if (this.maze[y][x] == 1) {
-      this.dispatch({
-        type: "UPDATE_HISTORY",
-        payload: {
-          command: `${command_name} ${xdiff} ${ydiff}`,
-          value: {
-            type: 'error',
-            value: `Wall ahead.`
-          },
-        }
-      });
-      return;
+    // Check for wall in the path of moving in x-direction
+    if (xdiff < 0) {
+      if (this.checkForWallInXdir(x, this.x, y)) {
+        this.dispatch({
+          type: "UPDATE_HISTORY",
+          payload: {
+            command: command_string,
+            value: {
+              type: 'error',
+              value: `You cannot cross a wall.`
+            },
+          }
+        });
+        return;
+      }
+    } else {
+      if (this.checkForWallInXdir(this.x, x, y)) {
+        this.dispatch({
+          type: "UPDATE_HISTORY",
+          payload: {
+            command: command_string,
+            value: {
+              type: 'error',
+              value: `You cannot cross a wall.`
+            },
+          }
+        });
+        return;
+      }
+    }
+
+    // Check for wall in the path of moving in y-direction
+    if (ydiff < 0) {
+      if (this.checkForWallInYdir(y, this.y, x)) {
+        this.dispatch({
+          type: "UPDATE_HISTORY",
+          payload: {
+            command: command_string,
+            value: {
+              type: 'error',
+              value: `You cannot cross a wall.`
+            },
+          }
+        });
+        return;
+      }
+    } else {
+      if (this.checkForWallInYdir(this.y, y, x)) {
+        this.dispatch({
+          type: "UPDATE_HISTORY",
+          payload: {
+            command: command_string,
+            value: {
+              type: 'error',
+              value: `You cannot cross a wall.`
+            },
+          }
+        });
+        return;
+      }
     }
 
     const output = {
-      command: `${command_name} ${xdiff} ${ydiff}`,
+      command: command_string,
       value: `Moved ${xdiff}, ${ydiff} pixels`,
     }
     this.dispatch({ type: "UPDATE_POSITON", payload: { x, y }})
